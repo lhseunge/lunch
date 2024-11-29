340f692b5605ceee8a5f72a332b5f51a5f7d8191
lhseunge <lhseunge@gmail.com>
2024-11-29 21:20:22 +0900

    매장 추가 valiation 추가

--- src/main/java/com/fun/lunch/dto/StoreRequest.java
+++ src/main/java/com/fun/lunch/dto/StoreRequest.java
@@ -1,10 +1,17 @@
 package com.fun.lunch.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record StoreRequest(
+        @NotNull
         String name,
         String description,
+        @NotNull
         double latitude,
+        @NotNull
         double longitude
 ) {
-
+    public StoreRequest(String name, double latitude, double longitude) {
+        this(name, "", latitude, longitude);
+    }
 }
