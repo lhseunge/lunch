4121538faea614b25605d9b287ac25c43e6efe73
lhseunge <lhseunge@gmail.com>
2024-12-06 10:49:39 +0900

    에러코드 num - 코드 수정

--- src/main/java/com/fun/lunch/exception/CustomExceptionEnum.java
+++ src/main/java/com/fun/lunch/exception/CustomExceptionEnum.java
@@ -1,4 +1,4 @@
-package com.fun.lunch.config;
+package com.fun.lunch.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,8 +10,9 @@ import org.springframework.http.HttpStatus;
 @ToString
 public enum CustomExceptionEnum {
 
-    EXIST_STORE(HttpStatus.BAD_REQUEST, "S001", "이미 저장된 가게입니다.");
+    EXIST_STORE(HttpStatus.BAD_REQUEST, "E001", "이미 저장된 가게입니다."),
 
+    ;
     private final HttpStatus httpStatus;
     private final String errorCode;
     private final String message;
