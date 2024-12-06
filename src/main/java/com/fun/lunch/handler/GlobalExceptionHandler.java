80588ebfb1af0d3615fac74961856e68b3f0d361
lhseunge <lhseunge@gmail.com>
2024-12-06 10:50:19 +0900

    exception handler - Exception.class 추가

--- src/main/java/com/fun/lunch/handler/GlobalExceptionHandler.java
+++ src/main/java/com/fun/lunch/handler/GlobalExceptionHandler.java
@@ -1,17 +1,30 @@
 package com.fun.lunch.handler;
 
-import com.fun.lunch.dto.ResponseException;
+import com.fun.lunch.exception.ResponseException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResponseException.class)
     public ResponseEntity<?> exception(ResponseException exception) {
         return ResponseEntity
                 .status(exception.getHttpStatus())
+                .body(exception);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> exception(Exception exception) {
+
+        log.error(exception.getMessage(), exception);
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exception);
     }
 }
