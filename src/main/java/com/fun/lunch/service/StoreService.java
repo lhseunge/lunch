d5cea0d5a8efbf60fdc99f5d9f16a6a964a59df0
lhseunge <lhseunge@gmail.com>
2024-12-06 20:18:54 +0900

    interface 캐시 어노테이션 제거

--- src/main/java/com/fun/lunch/service/StoreService.java
+++ src/main/java/com/fun/lunch/service/StoreService.java
@@ -3,13 +3,11 @@ package com.fun.lunch.service;
 import com.fun.lunch.dto.StoreRequest;
 import com.fun.lunch.dto.StoreResponse;
 import com.fun.lunch.entity.Store;
-import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
 public interface StoreService {
 
-    @Cacheable(value = "personalKey")
     List<StoreResponse> getStores(String personalKey);
 
     StoreResponse getRandomStores();
