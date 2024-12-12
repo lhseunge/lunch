ce31107af1144fffd2927e5798db1d120338c9fb
lhseunge <lhseunge@gmail.com>
2024-12-12 16:01:48 +0900

    JwtTokenProvider - pemKey value 사용하도록 수정

--- src/main/java/com/fun/lunch/service/impl/JwtTokenProvider.java
+++ src/main/java/com/fun/lunch/service/impl/JwtTokenProvider.java
@@ -3,8 +3,11 @@ package com.fun.lunch.service.impl;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -14,24 +17,33 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    @Value("${works.botId}")
-    private String botId;
-
-    @Value("${works.clientId}")
+    @Value("${works.auth.clientId}")
     private String clientId;
 
-    @Value("${works.serviceAccount}")
+    @Value("${works.auth.serviceAccount}")
     private String serviceAccount;
-
-    private final PrivateKey privateKey;
 
     @Value("${jwt.expiration_time}")
     private long accessTokenExpTime;
 
+    private final PrivateKey privateKey;
 
     public JwtTokenProvider(
-            @Value("${jwt.privateKey}") String privateKey
+            @Value("${jwt.privateKeyPath}") String privateKeyPath
     ) throws Exception {
+
+        ClassPathResource resource = new ClassPathResource(privateKeyPath);
+
+        byte[] bdata = FileCopyUtils.copyToByteArray(resource.getInputStream());
+
+        String privateKey = new String(bdata, StandardCharsets.UTF_8)
+                .replaceAll("-----BEGIN PRIVATE KEY-----","")
+                .replaceAll("-----END PRIVATE KEY-----", "")
+                .replaceAll(System.lineSeparator(), "")
+                .trim();
+
+
+
         byte[] keyBytes = Decoders.BASE64.decode(privateKey);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         this.privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(keyBytes));
