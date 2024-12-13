e68ef64fcb5fd8d643b6c136b159193cf7d082ca
lhseunge <lhseunge@gmail.com>
2024-12-13 18:35:43 +0900

    privateKey resouece 경로로 이동 및 사용 구현

--- src/main/java/com/fun/lunch/controller/WorksController.java
+++ src/main/java/com/fun/lunch/controller/WorksController.java
@@ -1,9 +1,14 @@
 package com.fun.lunch.controller;
 
 import com.fun.lunch.service.impl.JwtTokenProvider;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/works")
@@ -13,6 +18,21 @@ public class WorksController {
 
     public WorksController(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @GetMapping("/pem")
+    public String getPemValue() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("keys/works_private.key");
+
+        byte[] bdata = FileCopyUtils.copyToByteArray(resource.getInputStream());
+
+        return new String(bdata, StandardCharsets.UTF_8)
+                .replaceAll("-----BEGIN PRIVATE KEY-----","")
+                .replaceAll("-----END PRIVATE KEY-----", "")
+                .replaceAll(System.lineSeparator(), "")
+                .trim();
+
     }
 
     @GetMapping("/redirect")
