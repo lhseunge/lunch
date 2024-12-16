7a4302872d55abfb1c8f13f28e3f02b046585970
lhseunge <lhseunge@gmail.com>
2024-12-16 19:15:27 +0900

    works api 예외처리 추가

--- src/main/java/com/fun/lunch/exception/CustomExceptionEnum.java
+++ src/main/java/com/fun/lunch/exception/CustomExceptionEnum.java
@@ -13,7 +13,8 @@ public enum CustomExceptionEnum {
     EXIST_STORE(HttpStatus.BAD_REQUEST, "E001", "이미 저장된 가게입니다."),
     EMPTY_STORE_DATA(HttpStatus.BAD_REQUEST, "E002", "저장된 가게가 없습니다."),
 
-    EMPTY_STORE_DATA(HttpStatus.BAD_REQUEST, "E002", "저장된 가게가 없습니다.")
+
+    WORKS_EXCEPTION(HttpStatus.BAD_REQUEST, "W001", "works API Exception."),
 
     ;
     private final HttpStatus httpStatus;
