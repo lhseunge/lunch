193e478da3798bfe5187b9b0570b252badf541db
lhseunge <lhseunge@gmail.com>
2024-12-16 18:58:44 +0900

    works controller 메소드 정리

--- src/main/java/com/fun/lunch/controller/WorksController.java
+++ src/main/java/com/fun/lunch/controller/WorksController.java
@@ -1,44 +1,44 @@
 package com.fun.lunch.controller;
 
+import com.fun.lunch.dto.WorksAccessToken;
+import com.fun.lunch.service.WorksService;
 import com.fun.lunch.service.impl.JwtTokenProvider;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
+import com.fun.lunch.service.impl.WorksApi;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/works")
 public class WorksController {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final WorksService worksService;
 
-    public WorksController(JwtTokenProvider jwtTokenProvider) {
+    public WorksController(JwtTokenProvider jwtTokenProvider, WorksService worksService) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.worksService = worksService;
     }
 
-    @GetMapping("/pem")
-    public String getPemValue() throws IOException {
-
-        ClassPathResource resource = new ClassPathResource("keys/works_private.key");
-
-        byte[] bdata = FileCopyUtils.copyToByteArray(resource.getInputStream());
-
-        return new String(bdata, StandardCharsets.UTF_8)
-                .replaceAll("-----BEGIN PRIVATE KEY-----","")
-                .replaceAll("-----END PRIVATE KEY-----", "")
-                .replaceAll(System.lineSeparator(), "")
-                .trim();
-
-    }
-
-    @GetMapping("/redirect")
-    public String redirect() {
+    @GetMapping("/assertion")
+    public String assertion() {
 
         return jwtTokenProvider.createAccessToken();
+
+    }
+
+    @GetMapping("/access-token")
+    public WorksAccessToken getAccessToken() {
+
+        return worksService.getAccessToken();
+
+    }
+
+    @PostMapping("push")
+    public void sendWorksBotMessage() {
+
+        worksService.sendBotMessage();
 
     }
 }
