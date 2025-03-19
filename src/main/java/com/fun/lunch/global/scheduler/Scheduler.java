6c9a43ff09aa285acb0adb6a928e32b5445a2663
lhseunge <lhseunge@gmail.com>
2025-03-19 16:55:22 +0900

    Scheduler.java - 생성자 코드 제거 후 @RequiredArgsConstructor 추가

--- src/main/java/com/fun/lunch/global/scheduler/Scheduler.java
+++ src/main/java/com/fun/lunch/global/scheduler/Scheduler.java
@@ -1,19 +1,15 @@
 package com.fun.lunch.global.scheduler;
 
 import com.fun.lunch.domain.works.service.WorksService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class Scheduler {
 
     private final WorksService worksService;
-
-    public Scheduler(
-            WorksService worksService
-    ) {
-        this.worksService = worksService;
-    }
 
     @Scheduled(cron = "0 20 10 * * Mon-Fri")
     public void sendTodayLunch() {
